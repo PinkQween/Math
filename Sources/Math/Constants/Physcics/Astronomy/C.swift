495a128045d987a2aa2f55e39ad9d70733b7b526
Hanna Skairipa <hanna@hannaskairipa.com>
2025-09-20 13:15:58 -0600

    Cleaned up how constants work

---
+++ Sources/Math/Constants/Physcics/Astronomy/C.swift
@@ -0,0 +1,10 @@
+//
+//  H₀.swift
+//  Math
+//
+//  Created by Hanna Skairipa on 9/20/25.
+//
+
+public extension Math {
+    static let C: Math = .init(floatLiteral: 299792458) // m/s
+}
---
+++ Sources/Math/Constants/Physcics/C.swift
@@ -0,0 +1,12 @@
+//
+//  H₀.swift
+//  Math
+//
+//  Created by Hanna Skairipa on 9/20/25.
+//
+
+public extension Math {
+    static let H: Math = .init(floatLiteral: 72.6)
+    static let H₀ = H
+    static let hubbelsConstant = H
+}
